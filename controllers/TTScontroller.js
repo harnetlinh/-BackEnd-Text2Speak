2bfde9d8cbc35eac344bb63cc2875838a687baa1
harnetlinh <harnet.linh@gmail.com>
2020-12-03 23:02:29 +0700

    [ok]

--- controllers/TTScontroller.js
+++ controllers/TTScontroller.js
@@ -1,9 +1,10 @@
 	
   require('dotenv').config();
-  
+  var serveStatic = require('serve-static')
   const _ = require('lodash');
   const fs = require('fs');
-  
+  const path = require('path');
+
   const textToSpeech = require('@google-cloud/text-to-speech');
   
   const client = new textToSpeech.TextToSpeechClient();
@@ -21,21 +22,40 @@
     // The audio encoding type
     audioConfig: { audioEncoding: 'MP3' },
   };
+  const directory = 'samples';
+
+  function DeleteAllSamples(){
+    fs.readdir(directory, (err, files) => {
+      if (err) throw err;
   
-  const outputFileName = 'output.mp3';
-  
+      for (const file of files) {
+        fs.unlink(path.join(directory, file), err => {
+          if (err) throw err;
+        });
+      }
+    });
+  };
   
     exports.getAudio = (req,res)=>{
         client.synthesizeSpeech(request)
         .then(async (response) => {
-        console.log(response);
+        // console.log(response);
+        var test = response[0].audioContent;
         const audioContent = _.get(response[0], 'audioContent');
-    
+        var currentdate = new Date(); 
+        var datetime = "audio_" + currentdate.getDate() +
+          + (currentdate.getMonth()+1)  + 
+          + currentdate.getFullYear() +  
+          + currentdate.getHours() + 
+          + currentdate.getMinutes() +
+          + currentdate.getSeconds();
+        var nameFile = datetime+'.mp3';
+        var outputFileName = './public/'+nameFile;
         if (audioContent) {
-            fs.writeFileSync(outputFileName, audioContent, 'binary');
-    
+          fs.writeFileSync(outputFileName, audioContent, 'binary')
+          res.json({mp3:'http://localhost:3000/mp3/'+nameFile});
+          DeleteAllSamples();
             console.log(`Audio content successfully written to file: ${outputFileName}`);
-            res.status(200).json({"mp3":audioContent});
         } else {
             console.log('Failed to get audio content');
         }
--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,5 @@
 const router = require('express').Router();
 const Controller = require("../controllers/TTScontroller.js");
-
-// router.get("/getMp3WihtTimestam",Controller.getMp3WihtTimestam);
-// router.get("/getOnlyMp3",Controller.getOnlyMP3);
-// router.post("/callbackurl",Controller.CallBackUrl);
-router.get("/test",Controller.getAudio)
+router.post("/text2speech",Controller.getAudio)
+router.post("/feedback",Controller.getAudio)
 module.exports = router;
